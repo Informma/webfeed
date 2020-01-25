82dd26cd2bf81983940be556aabd9f557a8e0c35
tom <tom.craddock@informma.com>
2020-01-25 15:44:29 +1000

    Added links to person

--- lib/domain/atom_person.dart
+++ lib/domain/atom_person.dart
@@ -1,3 +1,4 @@
+import 'package:webfeed/domain/atom_link.dart';
 import 'package:webfeed/util/helpers.dart';
 import 'package:xml/xml.dart';
 
@@ -5,13 +6,17 @@ class AtomPerson {
   final String name;
   final String uri;
   final String email;
+  final List<AtomLink> links;
 
-  AtomPerson(this.name, this.uri, this.email);
+  AtomPerson(this.name, this.uri, this.email, this.links);
 
   factory AtomPerson.parse(XmlElement element) {
     var name = findElementOrNull(element, "name")?.text;
     var uri = findElementOrNull(element, "uri")?.text;
     var email = findElementOrNull(element, "email")?.text;
-    return AtomPerson(name, uri, email);
+    var links = element.findElements("link").map((element) {
+      return AtomLink.parse(element);
+    }).toList();
+    return AtomPerson(name, uri, email, links);
   }
 }
